59c978edbf4ccece17f00739b393cf92dedd2fe7
Q-RWorker <aqsha8amanah@gmail.com>
2025-06-29 05:58:20 +0700

    Fundamental Dasar

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari :
+1. Sekuensial : Langkah Berurutan
+2. Percabangan : Langkah Melompat Jika Kondisi Terpenuhi
+3. Perulangan : Mengulang Langkah Yang Sama Berkali-kali Selama / Sampai Kondisi Terpenuhi
+"""
+
+
+#Sekuensial
+print('Ibu berkata, "Pergi ke toko"') #Model Penulisan Kode 1
+print("Ibu berkata, \"Pergi ke toko\"") #Model Penulisan Kode 2
+
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko ?"')
+
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+
+print("Maka Budi berangkat ke toko")
+
+print("Dan mulai berbelanja")
