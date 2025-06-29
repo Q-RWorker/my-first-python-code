0969a758c176d92db208f28423dd22af85e2260d
Q-RWorker <aqsha8amanah@gmail.com>
2025-06-30 05:27:37 +0700

    Fundamental Dasar

--- main.py
+++ main.py
@@ -17,3 +17,37 @@ print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("Maka Budi berangkat ke toko")
 
 print("Dan mulai berbelanja")
+
+print("\n\n\n")
+###########################################################################
+#Tugas 1
+print('Ibu berkata, "Beli 1 botol susu"')
+print('Ibu berkata, "Jika ada telur, beli juga 6 butir telur"')
+print('Anak menjawab, "OK"')
+print("Anak berangkat ke toko")
+
+ada_susu = True
+print('Anak bertanya, "Apakah ada Susu?"')
+if ada_susu :
+    print('Penjaga toko menjawab, "Ada, harganya 5000 untuk 1 botol"')
+    print("Anak mengecek harganya, cukup")
+    print('Anak berkata, "Apakah ada telur?"')
+    ada_telur = False
+    if ada_telur :
+        print('Penjaga toko menjawab, "Ada telurnya"')
+        print("Anak membeli 1 botol susu dan 6 butir telur")
+    else:
+        print('Penjaga toko menjawab, "Maaf, telurnya habis"')
+        print("Anak membeli 1 botol susu saja")
+else:
+    print('Penjaga toko menjawab, "Tidak ada susu"')
+    print("Anak tidak jadi membeli susu")
+
+print("Anak pulang kerumah")
+
+if ada_susu:
+    print("Anak menyerahkan hasil belanjanya kepada Ibu")
+else:
+    print('Anak berkata, "Susunya tidak ada"')
+
+
